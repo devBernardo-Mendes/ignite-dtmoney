caaed5ed5cb3ef9ffa27094cf5bc564a5120f1d9
devBernardo-Mendes <devbernardomr@gmail.com>
2022-06-07 23:33:49 -0300

    feat: :sparkles: Import table for Dashboard

--- src/components/DashBoard/index.tsx
+++ src/components/DashBoard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { Table } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <Table />
     </Container>
   );
 }
