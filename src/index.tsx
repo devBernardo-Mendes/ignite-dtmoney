a8e259185ef0b6e9722908b6b89d8aed4b8c1eda
devBernardo-Mendes <devbernardomr@gmail.com>
2022-06-08 21:32:04 -0300

    feat: :sparkles: Create route for api miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createServer } from "miragejs";
 import App from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return {
+        id: 1,
+        title: "transaction 1",
+        amount: 400,
+        type: "deposit",
+        category: "food",
+        create_at: new Date(),
+      };
+    });
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
