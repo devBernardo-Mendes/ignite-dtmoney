201229d667d8b79b3ff46d22a6ce124479ca4336
devBernardo-Mendes <devbernardomr@gmail.com>
2022-06-20 23:47:07 -0300

    feat: :sparkles: Create interface for call function of father for children

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import logoImg from "../../assets/logo.svg";
+
 import { Container, Content } from "./styles";
 
-function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
