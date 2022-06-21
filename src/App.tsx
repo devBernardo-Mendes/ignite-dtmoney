e02088a5545c19fd77c8e8f339e6ecd5502f5d17
devBernardo-Mendes <devbernardomr@gmail.com>
2022-06-20 23:46:05 -0300

    feat: :sparkles: Create states and fucntions for open and close Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import { Dashboard } from "./components/DashBoard";
 import Header from "./components/Header";
+import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 function App() {
+  const [isNewTranslationModalOpen, setIsNewTranslationModalOpen] =
+    useState(false);
+
+  function handleOpenNewTranslationModal() {
+    setIsNewTranslationModalOpen(true);
+  }
+
+  function handleCloseNewTranslationModal() {
+    setIsNewTranslationModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTranslationModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTranslationModalOpen}
+        onRequestClose={handleCloseNewTranslationModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
