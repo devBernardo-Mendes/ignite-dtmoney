c24fecdf8bfe5a42b0b50148da248fe023900643
devBernardo-Mendes <devbernardomr@gmail.com>
2022-06-21 23:50:44 -0300

    feat: :sparkles: Create style global of close modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -72,8 +72,17 @@ max-width: 576px;
 background: var(--background);
 padding: 3rem;
 position: relative;
-border-radius: 0.25rem;
+border-radius: 0.24rem;
 
 }
+.react-modal-close{
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border:0;
+    background: transparent;
+}
+
+
 
 `;
