aef09a44cb100e7fab1d078d193513cd0d851b64
devBernardo-Mendes <devbernardomr@gmail.com>
2022-07-12 08:58:26 -0300

    feat: :sparkles: Create button the type

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -30,6 +32,19 @@ export function NewTransactionModal({
         <h2>Cadastrar transação</h2>
         <input placeholder="Título"></input>
         <input type="number" placeholder="Valor"></input>
+
+        <TransactionTypeContainer>
+          <button type="button" onClick={onRequestClose}>
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button" onClick={onRequestClose}>
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria"></input>
         <button type="submit">Cadastrar</button>
       </Container>
